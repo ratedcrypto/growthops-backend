cc8fd867315122339284791f76bdded1229767ab
Rujul Trivedi <rujultrivedi@Rujuls-MacBook-Pro-3.local>
2022-01-17 15:49:05 +1100

    feature(unit-test): get plant unit test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -10,3 +10,20 @@ it('can fetch plants', function () {
     $response = $this->getJson("/api/v1/plants");
     $this->assertCount(5, $response->json()['data']['data']);
 });
+
+it('can fetch a plant', function () {
+    $plant = Plant::factory()->create();
+    $response = $this->getJson("/api/v1/plants/{$plant->id}");
+    $data = [
+        'status' => 'success',
+        'data' => [
+            'id' => $plant->id,
+            'name' => $plant->name,
+            'species' => $plant->species,
+            'watering_instructions' => $plant->watering_instructions,
+            'photo' => $plant->photo
+        ]
+    ];
+
+    $response->assertStatus(200)->assertJson($data);
+});
