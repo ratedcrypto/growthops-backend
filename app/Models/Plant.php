cc393ccfb9e1edcbe8c3eb1103ff070680b5d3f5
Rujul Trivedi <rujultrivedi@Rujuls-MacBook-Pro-3.local>
2022-01-17 15:19:15 +1100

    fix(model): plant model soft delete

--- app/Models/Plant.php
+++ app/Models/Plant.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * @property int $id
@@ -16,7 +17,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Plant extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
