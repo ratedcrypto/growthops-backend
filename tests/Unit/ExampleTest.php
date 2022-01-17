3fc175dac94f51d25345894f3801a3efda8fb1d6
Rujul Trivedi <rujultrivedi@Rujuls-MacBook-Pro-3.local>
2022-01-17 15:30:27 +1100

    feature(unit-test): get plants unit test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,3 +1,12 @@
 <?php
 
-test('basic')->assertTrue(true);
+use App\Models\Plant;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(Tests\TestCase::class, RefreshDatabase::class);
+
+it('can fetch plants', function () {
+    Plant::factory()->times(5)->create();
+    $response = $this->getJson("/api/v1/plants");
+    $this->assertCount(5, $response->json()['data']['data']);
+});
